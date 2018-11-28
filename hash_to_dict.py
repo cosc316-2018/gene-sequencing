648ef6e6f247030a188190d8c4ca4029e991a9d9
Aaron George <angeorge200@gmail.com>
2018-11-28 00:12:34 -0500

    Added a function that converts the hash table to a dictionary, added print statements to verify that the finctions are working correctly

--- hash_to_dict.py
+++ hash_to_dict.py
@@ -1,23 +1,3 @@
-def convert_introns():
-    #the GOAL of this is to put the hash table data into a dictionary,,,,i don't actually know if its working
-    for name in range(len(segments)):
-        data_wanted=intron_hash.get(name)
-        #print(data_wanted)
-        intron_dict={}
-        #print("key? ",name)
-        if data_wanted is not None:
-            intron_dict[name]=data_wanted
-            #print(intron_dict[name])
-
-
-def convert_exons():
-    for name in range(len(exon_hash)):
-        data_wanted=exon_hash.get(name)
-        exon_dict={}
-        #dict_key = exon_hash.__getitem__(name)
-        if data_wanted is not None:
-            exon_dict[name]=data_wanted
-
-    #for i in range(len(intron_hash)):
-    #    print(intron_dict[i])
-    #print(exon_dict)
+def convert(hash_table):
+        dictionary = {hash_table.slots[x]:hash_table.data[x] for x in range(len(hash_table.slots)) if hash_table.slots[x] is not None}
+        return dictionary
--- main.py
+++ main.py
@@ -1,15 +1,20 @@
-from hash_table import HashTable as HT
+from hash_table import HashTable
 from sequence import generate_sequence
 from extraction import get_intron_exon
-from hash_to_dict import convert_introns, convert_exons
+import hash_to_dict
 
+# generate a sequence of 200 elements
 seq = generate_sequence()
 
 # split into segments of length 10
 segments = [[seq[i + j] for j in range(10)] for i in range(0, len(seq), 10)]
 
-intron_hash = HT()
-exon_hash = HT()
+# initialize the empty hash tables that will
+# hold the extracted introns and exons
+intron_hash = HashTable()
+exon_hash = HashTable()
+
+print('\n The sublists that were extracted from the initial gene sequence \n')
 
 i = 1
 for sublist in segments:
@@ -21,7 +26,18 @@ for sublist in segments:
     exon_hash.put(exons[1][0],exons[1][1])
     i += 1
 
+print('_' * 80)
+print('\n NOTE: The sizes listed do not include slots or data values that are \'None\'')
+print('\n', "INTRON HASH TABLE SLOTS", "( Size", len(intron_hash), ") --- ",intron_hash.slots)
+print('\n', "INTRON HASH TABLE DATA", "( Size", len(intron_hash), ") --- ",intron_hash.data)
+print('_' * 80)
+print('\n', "EXON HASH TABLE SLOTS", "( Size", len(exon_hash), ") --- ",exon_hash.slots)
+print('\n', "EXON HASH TABLE DATA", "( Size", len(exon_hash), ") --- ",exon_hash.data)
+print('_' * 80)
 
-print('\n', "intron hash table --- ",intron_hash.data)
-print('\n', "exon hash table slots --- ",exon_hash.slots)
-print('\n', "exon hash table --- ",exon_hash.data)
+intron_dictionary = hash_to_dict.convert(intron_hash)
+exon_dictionary = hash_to_dict.convert(exon_hash)
+
+print('\n', 'INTRON DICTIONARY', '( Size', len(intron_dictionary), ') ---', intron_dictionary)
+print('\n', 'EXON DICTIONARY', '( Size', len(exon_dictionary), ') ---', exon_dictionary)
+print('_' * 80)
